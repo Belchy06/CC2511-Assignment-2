c984baca152acdd584ab69f2c86f1c67abb787d3
Belchy06 <william.belcher@hotmail.com>
2020-10-29 21:57:42 +1000

    Remove matlab code

--- Code/MATLAB/joystick.m
+++
@@ -1,6 +0,0 @@
-while(1)
-    [pos, but] = mat_joy(0);
-    disp("Buttons \r\n");
-    disp(but);
-    pause(1);
-end
--- Code/MATLAB/serialtest.m
+++
@@ -1,5 +0,0 @@
-device = serialport("COM5",9600);
-configureTerminator(device,"CR");
-writeline(device,"X100");
-writeline(device,"X0");
-clear device;
